cba6a192e5fa2b574f7b16273b35566650378cc5
A Sudarsan Krishna <askrish2803@gmail.com>
2025-07-22 14:33:00 +0530

    Add files via upload

---
+++ Narrow.java
@@ -0,0 +1,10 @@
+public class Narrow {
+public static void main(String args[]) {
+	double d=100.245;
+	short j=(short)d;
+	int k=(int)d;
+	System.out.println("Original value: "+d);
+	System.out.println("Short: "+j);
+	System.out.println("Integer: "+k);
+}
+}
---
+++ Widening.java
@@ -0,0 +1,10 @@
+public class Widening {
+public static void main (String args[]) {
+	int i=10;
+	long l=i;
+	double d=i;
+	System.out.println("Integer: "+i);
+	System.out.println("Long: "+l);
+	System.out.println("double: "+d);
+}
+}
